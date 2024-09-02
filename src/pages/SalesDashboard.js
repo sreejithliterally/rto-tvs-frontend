125f9f4e63818dcc02aecb1d078e08591b059143
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-09-03 00:45:40 +0530

    magic

---
+++ src/pages/SalesDashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const SalesDashboard = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        {/* Main content for the Sales Dashboard will go here */}
+        <h1>Sales Dashboard</h1>
+      </div>
+    </div>
+  );
+};
+
+export default SalesDashboard;
