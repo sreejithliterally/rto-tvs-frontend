8a33280a2f3eee5ccaaf18297b424ff6ab932776
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-09-15 09:11:58 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SalesExecutiveHome from './pages/SalesExecutiveHome';
+import SalesExecutive from './pages/SalesExecutive';
 import NewCustomer from './pages/NewCustomer';
 import Dashboard from './pages/SalesDashboard';
 import CustomerInfo from './pages/CustomerInfo';
 import PdfEditor from './pages/PdfEditor';
 import Login from './pages/Login';
 import Admin from './pages/Admin';
-
+import Accounts from './pages/Accounts';
+import RTO from './pages/RTO';
+import Manager from './pages/Manager';
 
 const App = () => {
   return (
     <Router>
-      {/* Ensure Navbar is rendered */}
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path='/admin' element={<Admin/>}/>
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/sales-executive' element={<SalesExecutive />} />
+        <Route path='/accounts' element={<Accounts />} />
+        <Route path='/rto' element={<RTO />} />
+        <Route path='/manager' element={<Manager />} />
         <Route path="/new-customer" element={<NewCustomer />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* Update this path to match URLs with query strings */}
         <Route path="/customer-form" element={<CustomerInfo />} />
-        <Route path='/pdf-editor' element={<PdfEditor/>} />
+        <Route path='/pdf-editor' element={<PdfEditor />} />
       </Routes>
     </Router>
   );
