4837e18f644f3070595ed3ac96fa2afc04918449
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-10-01 20:17:15 +0530

    adminss

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/AdminDashboard.css'; // Importing the updated CSS file
+import '../styles/AdminDashboard.css'; // Importing the CSS file for styles
 
 const Admin = () => {
   const user = JSON.parse(localStorage.getItem('user'));
