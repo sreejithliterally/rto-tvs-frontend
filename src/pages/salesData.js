2b1873ba9a8e3de222aca3182123093e4addb50a
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-10-03 04:29:39 +0530

    admin

---
+++ src/pages/salesData.js
@@ -0,0 +1,14 @@
+// salesData.js
+const salesData = [
+  { date: '2024-09-01', lastMonth: 120, thisMonth: 150 },
+  { date: '2024-09-02', lastMonth: 130, thisMonth: 160 },
+  { date: '2024-09-03', lastMonth: 110, thisMonth: 140 },
+  { date: '2024-09-04', lastMonth: 100, thisMonth: 130 },
+  { date: '2024-09-05', lastMonth: 150, thisMonth: 170 },
+  // Add more days as necessary
+  { date: '2024-09-30', lastMonth: 180, thisMonth: 200 },
+  { date: '2024-10-01', lastMonth: 190, thisMonth: 220 },
+  // Include more dates for the current month if necessary
+];
+
+export default salesData;
