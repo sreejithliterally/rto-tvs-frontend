869872505e0e1a1d8d0ac57057d725e1d32cebee
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-10-05 17:23:07 +0530

    pdf🚗

--- src/pages/RTODetails.js
+++ src/pages/RTODetails.js
@@ -101,7 +101,7 @@ const RTODetails = () => {
     formData.append('pdf', form21Pdf);
 
     try {
-      const response = await axios.post('https://13.127.21.70:8000/process_pdf/form21', formData, {
+      const response = await axios.post('http://13.127.21.70:8000/pdf/process_pdf/form21', formData, {
         responseType: 'blob',
       });
       setProcessedForm21(URL.createObjectURL(response.data));
@@ -119,7 +119,7 @@ const RTODetails = () => {
     formData.append('finance_company', financeCompany);
 
     try {
-      const response = await axios.post('https://13.127.21.70:8000/process_pdf/form20', formData, {
+      const response = await axios.post('http://13.127.21.70:8000/pdf/process_pdf/form20', formData, {
         responseType: 'blob',
       });
       setProcessedForm20(URL.createObjectURL(response.data));
@@ -136,7 +136,7 @@ const RTODetails = () => {
     formData.append('signature', buyerSignature);
 
     try {
-      const response = await axios.post('https://13.127.21.70:8000/process_pdf/invoice', formData, {
+      const response = await axios.post('http://13.127.21.70:8000/pdf/process_pdf/invoice', formData, {
         responseType: 'blob',
       });
       setProcessedInvoice(URL.createObjectURL(response.data));
